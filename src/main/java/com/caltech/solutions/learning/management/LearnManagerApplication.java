ae946dbbb73c2dbd65b773bee86ecd3882a2d946
Weverton Souza <wevertonad@gmail.com>
2020-02-27 21:44:08 -0300

    Removed security libs from project.

--- src/main/java/com/caltech/solutions/learning/management/LearnManagerApplication.java
+++ src/main/java/com/caltech/solutions/learning/management/LearnManagerApplication.java
@@ -3,12 +3,9 @@ package com.caltech.solutions.learning.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableMongoAuditing
 @SpringBootApplication
-@EnableResourceServer
 public class LearnManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearnManagerApplication.class, args);
--- src/main/java/com/caltech/solutions/learning/management/security/WebSecurityConfigure.java
+++ src/main/java/com/caltech/solutions/learning/management/security/WebSecurityConfigure.java
@@ -2,15 +2,10 @@ package com.caltech.solutions.learning.management.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,28 +19,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
-    @Override @Bean
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-
-    @Override @Bean
-    protected UserDetailsService userDetailsService() {
-        return super.userDetailsService();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests().anyRequest().authenticated()
-            .and().httpBasic()
-            .and().sessionManagement().disable();
+                .csrf()
+                .and()
+                .cors()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/v1/**")
+                .permitAll()
+                .and()
+                .sessionManagement()
+                .disable();
     }
 
     @Bean
@@ -57,14 +43,13 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter implement
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html",
-                "/webjars/**",
-                "/profiles/**",
-                "/clients/**"
-        );
+        web.ignoring()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**"
+                );
     }
 }
